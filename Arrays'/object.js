e0659c8094a3a80f055981e775f444698e95e324
yogaprasadk <yogaprasadf12@gmail.com>
2024-09-01 21:19:22 +0530

    arrays and objects basics completed

---
+++ Arrays'/object.js
@@ -0,0 +1,26 @@
+// create object
+let con = {
+          name:"yoga",
+          course:"cse"
+}
+console.log(con);
+// {name:'yoga',course:'cse'}
+
+// accessing single object
+let singleobj = {
+          name:"yogaprasad",
+          course:"cse",
+          age:"21"
+}
+console.log(singleobj.name)
+// 'yogaprasad'
+
+// functions inobjects
+const t = {
+          greet:function(){
+                    console.log("HelooWorld")
+          }
+}
+obj.greet();
+// HelooWorld
+
