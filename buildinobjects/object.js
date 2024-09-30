fcd91e8771fa4d1f08fb31f7066734305617151a
yogaprasadk <yogaprasadf12@gmail.com>
2024-09-30 08:05:56 +0530

    build in object completed

---
+++ buildinobjects/object.js
@@ -0,0 +1,44 @@
+// 1.Math Objects
+// PI VALUE
+console.log(Math.PI) //3.141592653589793
+
+// max
+console.log(Math.max(23,52,51,59))
+
+//min 
+console.log(Math.min(34,2,45,1,3))
+
+// round
+console.log(Math.round(34.2))
+
+// ceil
+console.log(Math.ceil(34.2))
+
+// floor
+console.log(Math.floor(49.6))
+
+// absolute
+console.log(Math.abs(-45.3))
+
+// random
+console.log(Math.random())
+
+// square root
+console.log(Math.sqrt(5))
+
+// power
+console.log(Math.pow(3,6))
+
+
+// 2. Date and Time Objects
+
+// date
+let curr = new Date()
+console.log(curr)
+
+// hour minutes seconds
+let currdate = new Date(2024,11,55,2)
+console.log(currdate)
+
+console.log(currdate.getDay())
+
