88d74dcd4b7eb772195ec294f535a47f64d66945
yogaprasadk <yogaprasadf12@gmail.com>
2024-10-17 21:07:27 +0530

    document with query and get element finished

---
+++ domhtml/index.js
@@ -0,0 +1,30 @@
+// DOM -DOCUMENT OBJECT MODEL
+
+// document
+// get element by id
+document.getElementById("fpara");
+
+// get element by class
+document.getElementsByClassName("cspa ra");
+
+// get element by tagname
+document.getElementsByTagName('p')
+
+// Query Selector
+// id
+document.querySelector("#fpara")
+
+// class
+document.querySelector(".cspara")
+
+// tag
+document.querySelector('p')
+ 
+// query selector All
+// tag
+document.querySelectorAll('p')
+// class
+document.querySelectorAll('.cspara')
+// id
+document.querySelectorAll('#fpara')
+
