5fef688fa65c404812f0b33938bc96d35619f611
yogaprasadk <yogaprasadf12@gmail.com>
2024-10-31 20:37:20 +0530

    eventloop completed

---
+++ Eventloop/index.js
@@ -0,0 +1,24 @@
+//  event loop
+
+// async should be in eventloop
+
+console.log("Start")
+
+setTimeout(function timeout(){
+          console.log("Click the buttom");
+},4000);
+
+console.log("End");
+
+
+/*
+call stack
+Browser
+call Back
+
+use loupe website for better understanding
+*/
+
+
+
+ 
